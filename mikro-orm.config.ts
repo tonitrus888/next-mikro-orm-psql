37d8b26c26b73fe627721d8a7a59e7a07a3b74d7
Benjamin Stolz <benjamin.stolz@outlook.at>
2024-10-15 23:12:44 +0200

    install mikro-orm migrations

--- mikro-orm.config.ts
+++ mikro-orm.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "@mikro-orm/postgresql";
 import { Test } from "./entities/test.entity";
+import { Migrator } from "@mikro-orm/migrations";
 
 export default defineConfig({
   entities: [Test],
+  extensions: [Migrator],
 });
